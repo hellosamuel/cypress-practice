ec205d988bf798873ee6e01ba61121f3fff6430b
Samuel Lee <lee.samuel@ai2-jp.com>
2022-05-13 11:40:51 +0900

    EDIT chagne config in CI and set browser in chrome

--- cypress/integration/1-getting-started/todo.spec.ts
+++ cypress/integration/1-getting-started/todo.spec.ts
@@ -119,7 +119,7 @@ describe('example to-do app', () => {
       cy.get('.todo-list li').should('have.length', 1).should('not.have.text', 'Pay electric bill')
 
       // Finally, make sure that the clear button no longer exists.
-      cy.contains('Clear completed').should('exist')
+      cy.contains('Clear completed').should('not.exist')
     })
   })
 })
